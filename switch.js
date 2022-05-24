58e8a1b4603de1e69357d6ca5eee4d6a8dc711db
eunsun <nammugmuhg@gmail.com>
2022-05-24 20:20:12 +0900

    다크모드 기능 완성

--- switch.js
+++ switch.js
@@ -1,24 +1,18 @@
-const toggleSwitch = document.querySelector(
-  '.switch input[type="checkbox"]'
-);
-const currentTheme = localStorage.getItem("theme");
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
-}
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 
 function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  }
+  let postcards = document.querySelector(".postcards");
+  let postcard = document.querySelectorAll(".postcards .postcard");
+  let modal_content = document.querySelector(".modal-content");
+  let navbar = document.querySelector(".navbar");
+  let menu = document.querySelectorAll(".navbar .menu h3");
+
+  postcards.classList.toggle("postcards-dark-mode");
+  postcard.forEach((x) => x.classList.toggle("postcard-dark-mode"));
+  modal_content.classList.toggle("postcard-dark-mode");
+  navbar.classList.toggle("navbar-dark-mode");
+  menu.forEach((x) => x.classList.toggle("navbar-dark-mode"));
+
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
